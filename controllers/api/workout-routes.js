80ad6f799f7d5589d9e9a806d3194367f2e77ae8
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-14 14:35:32 -0700

    GET range route

--- controllers/api/workout-routes.js
+++ controllers/api/workout-routes.js
@@ -12,6 +12,20 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/range', async (req, res) => {
+    try {
+        const response = await db.Workout.find({});
+        const range = []
+        for (let i = response.length-1; i>=0; i--) {
+            range.unshift(response[i]);
+        }
+        res.status(200).json(range)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
 router.get('/:id', async (req, res) => {
     try {
         // console.log(req);
@@ -45,6 +59,23 @@ router.put('/:id', async (req, res) => {
     }
 })
 
-
+router.post('/', async (req, res) => {
+    try {
+        const response = await db.Workout.create(
+            req.body,
+            (err, created) => {
+                if (err) {
+                    console.log(err);
+                    res.send(err);
+                } else {
+                    console.log(created);
+                    res.send(created);
+                }
+            }
+        )
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
