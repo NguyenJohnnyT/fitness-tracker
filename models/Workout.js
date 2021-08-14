356d2d224f2b2f36bd1fa86880fde1d8b9f2fadb
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-14 12:38:45 -0700

    complete seed

--- models/Workout.js
+++ models/Workout.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ExerciseSchema = new Schema({
+const WorkoutSchema = new Schema({
     type: {
       type: String,
       trim: true,
@@ -35,6 +35,6 @@ const ExerciseSchema = new Schema({
     },
   });
 
-  const Exercise = mongoose.model("Exercise", ExerciseSchema)
+  const Workout = mongoose.model("Workout", WorkoutSchema)
 
-  module.exports = Exercise;
+  module.exports = Workout;
