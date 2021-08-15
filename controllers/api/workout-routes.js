78b1d14c94b46e8b00efe9d66fae83df0b8d4c6c
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-14 17:43:55 -0700

    fixed range not returning totalduration/totaldistance

--- controllers/api/workout-routes.js
+++ controllers/api/workout-routes.js
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => { //TESTED
             // console.log('is this working..? ', workout.totalDuration);
             // console.log('workout ', workout);
         })
-        console.log('ALL workouts', workouts);
+        // console.log('ALL workouts', workouts);
 
         res.status(200).json(workouts);
     } catch (err) {
@@ -30,16 +30,28 @@ router.get('/', async (req, res) => { //TESTED
 router.get('/range', async (req, res) => { //TESTED
     try {
         const response = await db.Workout.find({});
-        const range = []
+        const rangeWorkouts = []
         var sevenDays = new Date().setDate(new Date().getDate()-7);
         for (let i = response.length-1; i>=0; i--) {
             // console.log('response[i], ', response[i])
             if (response[i].day > sevenDays) {
-                range.unshift(response[i]);
+                rangeWorkouts.unshift(response[i]);
             }
         }
-        // console.log(range);
-        res.status(200).json(range)
+        rangeWorkouts.forEach(workout => {
+            var totalDur = 0
+            var totalDis = 0
+            workout.exercises.forEach(exercise => {
+                // console.log('exercise', exercise);
+                totalDur += exercise.duration
+                totalDis += exercise.distance
+            });
+            
+            workout.totalDuration = totalDur
+            workout.totalDistance = totalDis
+        })
+        console.log(rangeWorkouts);
+        res.status(200).json(rangeWorkouts)
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
